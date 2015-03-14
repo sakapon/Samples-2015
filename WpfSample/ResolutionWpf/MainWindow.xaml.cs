cc039d2890339bc974b3aad740cd010007ff6e33
Keiho Sakapon <git@saka-pon.net>
2015-03-14 20:22:11 +0900

    Update layout

--- WpfSample/ResolutionWpf/MainWindow.xaml.cs
+++ WpfSample/ResolutionWpf/MainWindow.xaml.cs
@@ -20,9 +20,33 @@ namespace ResolutionWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static readonly DependencyProperty MouseOnWindowProperty =
+            DependencyProperty.Register("MouseOnWindow", typeof(Point), typeof(MainWindow), new PropertyMetadata(new Point()));
+
+        public static readonly DependencyProperty MouseOnScreenProperty =
+            DependencyProperty.Register("MouseOnScreen", typeof(Point), typeof(MainWindow), new PropertyMetadata(new Point()));
+
+        public Point MouseOnWindow
+        {
+            get { return (Point)GetValue(MouseOnWindowProperty); }
+            private set { SetValue(MouseOnWindowProperty, value); }
+        }
+
+        public Point MouseOnScreen
+        {
+            get { return (Point)GetValue(MouseOnScreenProperty); }
+            private set { SetValue(MouseOnScreenProperty, value); }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            MouseMove += (o, e) =>
+            {
+                MouseOnWindow = e.GetPosition(this);
+                MouseOnScreen = PointToScreen(MouseOnWindow);
+            };
         }
     }
 }
