cc693df58e2c1c63e2a48373a3fcdf651b8cd054
Keiho Sakapon <git@saka-pon.net>
2015-03-24 19:10:56 +0900

    Use ItemsControl and WrapPanel

---
+++ WpfSample/LayoutWpf/AppModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LayoutWpf
+{
+    public class AppModel
+    {
+        public int[] Numbers { get; private set; }
+
+        public AppModel()
+        {
+            Numbers = Enumerable.Range(1, 15).ToArray();
+        }
+    }
+}
