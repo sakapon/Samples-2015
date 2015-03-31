8e7a454089b41698b8a09569a38a15be3bda4b81
Keiho Sakapon <github@saka-pon.net>
2015-03-31 17:04:54 +0900

    Add AppModel class

---
+++ AzureMLSample/ColorClustersWpf/AppModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using KLibrary.Labs.Reactive.Models;
+
+namespace ColorClustersWpf
+{
+    public class AppModel
+    {
+        const string OutputDirPath = @"..\..\..\ColorData\Output";
+
+        public string[] OutputCsvs { get; private set; }
+        public IObservableProperty<string> SelectedOutputCsv { get; private set; }
+
+        public IObservableGetProperty<IDictionary<int, ColorInfo[]>> Assignments { get; private set; }
+
+        public AppModel()
+        {
+            OutputCsvs = Directory.EnumerateFiles(OutputDirPath)
+                .Select(Path.GetFileName)
+                .ToArray();
+            SelectedOutputCsv = ObservableProperty.Create(OutputCsvs.FirstOrDefault());
+
+            Assignments = SelectedOutputCsv.ToGetProperty(ReadAssignments);
+        }
+
+        static IDictionary<int, ColorInfo[]> ReadAssignments(string csvName)
+        {
+            var csvPath = Path.Combine(OutputDirPath, csvName);
+
+            var columns = File.ReadLines(csvPath)
+                .Take(1)
+                .SelectMany(l => l.Split(','))
+                .Select((c, i) => new { c, i })
+                .ToDictionary(_ => _.c, _ => _.i);
+
+            return File.ReadLines(csvPath)
+                .Skip(1)
+                .Select(l => l.Split(','))
+                .Select(r => new
+                {
+                    Assignments = Convert.ToInt32(r[columns["Assignments"]]),
+                    Info = new ColorInfo { Name = r[columns["Name"]], RGB = r[columns["RGB"]] },
+                    Color = ColorTranslator.FromHtml(r[columns["RGB"]]),
+                })
+                .GroupBy(_ => _.Assignments)
+                .OrderBy(g => g.Average(_ => _.Color.GetHue()))
+                .ToDictionary(g => g.Key, g => g.Select(_ => _.Info).ToArray());
+        }
+    }
+
+    public struct ColorInfo
+    {
+        public string Name { get; set; }
+        public string RGB { get; set; }
+    }
+}
