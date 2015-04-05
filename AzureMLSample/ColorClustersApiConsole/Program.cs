907c4f3907c4a3d918c15e8ef40553199f20bd13
Keiho Sakapon <git@saka-pon.net>
2015-04-05 22:30:02 +0900

    Add generated sample code

--- AzureMLSample/ColorClustersApiConsole/Program.cs
+++ AzureMLSample/ColorClustersApiConsole/Program.cs
@@ -1,15 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ColorClustersApiConsole
 {
+    public class StringTable
+    {
+        public string[] ColumnNames { get; set; }
+        public string[,] Values { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            InvokeRequestResponseService().Wait();
+        }
+
+        static async Task InvokeRequestResponseService()
+        {
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+                    Inputs = new Dictionary<string, StringTable>
+                    {
+                        {
+                            "input1",
+                            new StringTable
+                            {
+                                ColumnNames = new string[] {"RGB", "Name", "R", "G", "B", "Hue", "Saturation", "Brightness"},
+                                Values = new string[,] { { "value", "value", "0", "0", "0", "0", "0", "0" }, { "value", "value", "0", "0", "0", "0", "0", "0" }, }
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>(),
+                };
+                const string apiUri = "abc123"; // Replace this with the URI for the web service
+                const string apiKey = "abc123"; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                client.BaseAddress = new Uri(apiUri);
+
+                // WARNING: The 'await' statement below can result in a deadlock if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false) so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Result: {0}", result);
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                }
+            }
         }
     }
 }
