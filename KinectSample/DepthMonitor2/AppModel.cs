b5cef567260a320f61ba6b1259f3c0876539b0e4
Keiho Sakapon <git@saka-pon.net>
2015-04-26 13:54:24 +0900

    Add AppModel class

---
+++ KinectSample/DepthMonitor2/AppModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media.Imaging;
+using KLibrary.Labs.ObservableModel;
+
+namespace DepthMonitor2
+{
+    public class AppModel
+    {
+        public ISettableProperty<WriteableBitmap> DepthBitmap { get; private set; }
+
+        public AppModel()
+        {
+            DepthBitmap = ObservableProperty.CreateSettable<WriteableBitmap>(null);
+        }
+    }
+}
