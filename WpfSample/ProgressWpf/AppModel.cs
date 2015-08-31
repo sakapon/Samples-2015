081cdb517335eb83528763e2d440f6064540ae41
Keiho Sakapon <git@saka-pon.net>
2015-08-31 15:11:46 +0900

    Implement progress bars

---
+++ WpfSample/ProgressWpf/AppModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using KLibrary.Labs.ObservableModel;
+
+namespace ProgressWpf
+{
+    public class AppModel
+    {
+        public IGetOnlyProperty<double> ProgressValue { get; private set; }
+
+        public AppModel()
+        {
+            ProgressValue = Observable.Interval(TimeSpan.FromSeconds(0.05))
+                .Select(l => l % 101)
+                .Select(l => l / 100.0)
+                .ToGetOnly(0.0);
+        }
+    }
+}
