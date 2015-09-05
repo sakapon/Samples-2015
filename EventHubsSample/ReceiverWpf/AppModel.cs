a6e7ea6c19277a1d4b7236dd5879add62f8da9d4
Keiho Sakapon <git@saka-pon.net>
2015-09-05 23:44:22 +0900

    Add StaticEventProcessor class

--- EventHubsSample/ReceiverWpf/AppModel.cs
+++ EventHubsSample/ReceiverWpf/AppModel.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Linq;
 using System.Windows;
 using KLibrary.Labs.ObservableModel;
 using Microsoft.ServiceBus.Messaging;
@@ -18,8 +16,14 @@ namespace ReceiverWpf
 
         public AppModel()
         {
-            Position = SimpleEventProcessor.Message
-                .SelectToGetOnly(s => Point.Parse(s));
+            var index = -1;
+            Position = StaticEventProcessor.Message
+                .Select(m => (dynamic)JsonConvert.DeserializeObject(m))
+                .Where(_ => _.index > index)
+                .Do(_ => index = _.index)
+                .Select(_ => (string)_.position)
+                .Select(Point.Parse)
+                .ToGetOnly(default(Point));
 
             var hostName = string.Format("Host-{0:yyyyMMdd-HHmmss}", DateTime.Now);
             var eventHubName = "sakapon-event-201508";
@@ -28,57 +32,7 @@ namespace ReceiverWpf
 
             // Receives event once for one Consumer Group.
             var host = new EventProcessorHost(hostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
-            host.RegisterEventProcessorAsync<SimpleEventProcessor>();
-        }
-    }
-
-    class SimpleEventProcessor : IEventProcessor
-    {
-        public static ISettableProperty<string> Message { get; private set; }
-
-        static SimpleEventProcessor()
-        {
-            Message = ObservableProperty.CreateSettable("0,0");
-        }
-
-        async Task IEventProcessor.CloseAsync(PartitionContext context, CloseReason reason)
-        {
-            Debug.WriteLine("Processor Shutting Down. Partition '{0}', Reason: '{1}'.", context.Lease.PartitionId, reason);
-
-            if (reason == CloseReason.Shutdown)
-            {
-                await context.CheckpointAsync();
-            }
-        }
-
-        Task IEventProcessor.OpenAsync(PartitionContext context)
-        {
-            Debug.WriteLine("SimpleEventProcessor initialized. Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset);
-
-            return Task.FromResult<object>(null);
-        }
-
-        static int index = -1;
-        static readonly object indexLock = new object();
-
-        async Task IEventProcessor.ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
-        {
-            foreach (var data in messages)
-            {
-                lock (indexLock)
-                {
-                    var message = Encoding.UTF8.GetString(data.GetBytes());
-                    dynamic obj = JsonConvert.DeserializeObject(message);
-
-                    if (obj.index <= index) continue;
-                    index = obj.index;
-
-                    Message.Value = obj.position;
-
-                    Debug.WriteLine("Message received. Partition: '{0}', Data: '{1}'", context.Lease.PartitionId, message);
-                }
-            }
-            await context.CheckpointAsync();
+            host.RegisterEventProcessorAsync<StaticEventProcessor>();
         }
     }
 }
---
+++ EventHubsSample/ReceiverWpf/StaticEventProcessor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KLibrary.Labs.ObservableModel;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ReceiverWpf
+{
+    public class StaticEventProcessor : IEventProcessor
+    {
+        public static ISettableProperty<string> Message { get; private set; }
+
+        static StaticEventProcessor()
+        {
+            Message = ObservableProperty.CreateSettable<string>(null);
+        }
+
+        public async Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Debug.WriteLine("Processor closing. Partition '{0}', Reason: '{1}'", context.Lease.PartitionId, reason);
+
+            if (reason == CloseReason.Shutdown)
+                await context.CheckpointAsync();
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Debug.WriteLine("Processor opening. Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset);
+
+            return Task.FromResult<object>(null);
+        }
+
+        static readonly object messageLock = new object();
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (var data in messages)
+            {
+                lock (messageLock)
+                {
+                    var message = Encoding.UTF8.GetString(data.GetBytes());
+                    Debug.WriteLine("Message received. Partition: '{0}', Message: '{1}'", context.Lease.PartitionId, message);
+
+                    Message.Value = message;
+                }
+            }
+
+            await context.CheckpointAsync();
+        }
+    }
+}
