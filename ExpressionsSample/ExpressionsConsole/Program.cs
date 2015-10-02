665b1a4675afb67eb1a99a80c4e5ba6a01dd04c1
Keiho Sakapon <git@saka-pon.net>
2015-10-02 16:49:59 +0900

    Add test

--- ExpressionsSample/ExpressionsConsole/Program.cs
+++ ExpressionsSample/ExpressionsConsole/Program.cs
@@ -10,6 +10,33 @@ namespace ExpressionsConsole
     {
         static void Main(string[] args)
         {
+            EntityTest();
+            CsvTest();
+        }
+
+        static void EntityTest()
+        {
+            var PersonType = EntityType.Create(new { Id = 0, Name = "", Birthday = DateTime.MinValue });
+            // Uses a lambda expression.
+            //var PersonType = EntityType.Create(() => new { Id = 0, Name = "", Birthday = DateTime.MinValue });
+
+            var person1 = PersonType.CreateEntity(123, "Taro", new DateTime(2001, 1, 1));
+            var person2 = PersonType.CreateEntity(456, "Jiro", new DateTime(2002, 2, 2));
+
+            Console.WriteLine(person1);
+            Console.WriteLine(person2);
+        }
+
+        static void CsvTest()
+        {
+            var TaskItemType = EntityType.Create(new { Id = 0, Name = "", Rotation = DayOfWeek.Sunday, StartTime = TimeSpan.Zero });
+
+            var query = CsvFile.ReadEntities("tasks.csv", TaskItemType)
+                .Where(o => o.Rotation == DayOfWeek.Monday)
+                .OrderBy(o => o.StartTime);
+
+            foreach (var item in query)
+                Console.WriteLine("{0}: {1}", item.StartTime, item.Name);
         }
     }
 }
