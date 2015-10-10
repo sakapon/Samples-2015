bfd4537cf078966362dc267796e78f3547aa32e8
Keiho Sakapon <git@saka-pon.net>
2015-10-11 00:39:56 +0900

    Fix IO errors

--- BuildSample/Tools/CreateZip.ps1
+++ BuildSample/Tools/CreateZip.ps1
@@ -1,2 +1,6 @@
 ﻿[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
+
+$parent = [System.IO.Directory]::GetParent($Args[1])
+[System.IO.Directory]::CreateDirectory($parent)
+[System.IO.File]::Delete($Args[1])
 [System.IO.Compression.ZipFile]::CreateFromDirectory($Args[0], $Args[1])
