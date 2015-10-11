1a9ce97baaea6141e9a8d6e63b9949fc63f2a9d5
Keiho Sakapon <git@saka-pon.net>
2015-10-11 19:45:20 +0900

    Improve code

--- BuildSample/Tools/CreateZip.ps1
+++ BuildSample/Tools/CreateZip.ps1
@@ -5,7 +5,7 @@ if (-not ($Args[1])) { return 101 }
 
 [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
 
-$parent = [System.IO.Directory]::GetParent($Args[1])
+$parent = [System.IO.Path]::GetDirectoryName($Args[1])
 [System.IO.Directory]::CreateDirectory($parent)
 [System.IO.File]::Delete($Args[1])
 [System.IO.Compression.ZipFile]::CreateFromDirectory($Args[0], $Args[1])
