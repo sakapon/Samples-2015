02b2aa54fd12bd6ab3e399c9b2ed3450b08deb9c
Keiho Sakapon <git@saka-pon.net>
2015-10-11 22:54:55 +0900

    Create ZIP file with assembly version

---
+++ BuildSample/Tools/CreateZipForAssembly.ps1
@@ -0,0 +1,19 @@
+﻿# source assembly file path
+if (-not ($Args[0])) { return 100 }
+# target dir path
+if (-not ($Args[1])) { return 101 }
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
+
+$assemblyName = [System.IO.Path]::GetFileNameWithoutExtension($Args[0])
+$assembly = [System.Reflection.Assembly]::LoadFrom($Args[0])
+$assemblyFileVersion = [System.Reflection.CustomAttributeExtensions]::GetCustomAttribute($assembly, [System.Reflection.AssemblyFileVersionAttribute])
+if (-not ($assemblyFileVersion)) { return 200 }
+
+$sourceDirPath = [System.IO.Path]::GetDirectoryName($Args[0])
+$targetZipFileName = [string]::Format("{0}-{1}.zip", $assemblyName, $assemblyFileVersion.Version)
+$targetZipFilePath = [System.IO.Path]::Combine($Args[1], $targetZipFileName)
+
+[System.IO.Directory]::CreateDirectory($Args[1])
+[System.IO.File]::Delete($targetZipFilePath)
+[System.IO.Compression.ZipFile]::CreateFromDirectory($sourceDirPath, $targetZipFilePath)
