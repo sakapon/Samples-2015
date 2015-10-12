efdac988f2195d6399f97458d6aeed71894432be
Keiho Sakapon <git@saka-pon.net>
2015-10-12 17:38:18 +0900

    Add script for release build

---
+++ BuildSample/Tools/Build-BuildSample.ps1
@@ -0,0 +1,7 @@
+﻿$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+
+cd ..
+Invoke-Expression ($msbuild + " BuildSample.sln /p:Configuration=Release /t:Clean")
+Invoke-Expression ($msbuild + " BuildSample.sln /p:Configuration=Release /t:Rebuild")
+
+explorer Downloads
