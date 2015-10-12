b5146454580dc83d1bf22297f7803811655096e9
Keiho Sakapon <git@saka-pon.net>
2015-10-13 00:50:13 +0900

    Improve code

--- BuildSample/Tools/Build-BuildSample.ps1
+++
@@ -1,7 +0,0 @@
-﻿$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-
-$slnFilePath = "..\BuildSample.sln"
-Invoke-Expression ([string]::Format("{0} {1} /p:Configuration=Release /t:Clean", $msbuild, $slnFilePath))
-Invoke-Expression ([string]::Format("{0} {1} /p:Configuration=Release /t:Rebuild", $msbuild, $slnFilePath))
-
-explorer ..\Downloads
--- BuildSample/Tools/Build-EmptyConsole.ps1
+++ BuildSample/Tools/Build-EmptyConsole.ps1
@@ -4,10 +4,7 @@ $slnFilePath = "..\BuildSample.sln"
 Invoke-Expression ([string]::Format("{0} {1} /p:Configuration=Release /t:Clean", $msbuild, $slnFilePath))
 Invoke-Expression ([string]::Format("{0} {1} /p:Configuration=Release /t:Rebuild", $msbuild, $slnFilePath))
 
-cd ..\EmptyConsole
-xcopy Data bin\Release\Data /D/E/C/I/H/Y
-
-# In case of PowerShell, thw work dir is that of .ps1 file.
-..\Tools\CreateZipForAssembly.ps1 ..\EmptyConsole\bin\Release\EmptyConsole.exe ..\Downloads
+# In case of PowerShell, the work directory is that of .ps1 file.
+.\CreateZipForAssembly.ps1 ..\EmptyConsole\bin\Release\EmptyConsole.exe ..\Downloads
 
 explorer ..\Downloads
