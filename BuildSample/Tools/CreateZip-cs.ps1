1165f96d06171b31ac3e19a1fd7101a48cde6a5b
Keiho Sakapon <git@saka-pon.net>
2015-10-13 12:11:30 +0900

    Add PS script with C#

---
+++ BuildSample/Tools/CreateZip-cs.ps1
@@ -0,0 +1,25 @@
+﻿# source directory path
+if (-not ($Args[0])) { return 100 }
+# target zip file path
+if (-not ($Args[1])) { return 101 }
+
+$references = @("System.IO.Compression.FileSystem")
+$source = @"
+using System;
+using System.IO;
+using System.IO.Compression;
+
+public static class ZipHelper
+{
+    public static void CreateZip(string sourceDirPath, string targetZipFilePath)
+    {
+        var targetDirPath = Path.GetDirectoryName(targetZipFilePath);
+        Directory.CreateDirectory(targetDirPath);
+        File.Delete(targetZipFilePath);
+        ZipFile.CreateFromDirectory(sourceDirPath, targetZipFilePath);
+    }
+}
+"@
+
+Add-Type -TypeDefinition $source -Language CSharp -ReferencedAssemblies $references
+[ZipHelper]::CreateZip($Args[0], $Args[1])
