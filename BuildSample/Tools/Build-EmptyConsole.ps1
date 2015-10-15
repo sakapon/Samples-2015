d0128cf19eabb8863bf30b5166af587a29ac4802
Keiho Sakapon <git@saka-pon.net>
2015-10-15 15:05:22 +0900

    Increment version

--- BuildSample/Tools/Build-EmptyConsole.ps1
+++ BuildSample/Tools/Build-EmptyConsole.ps1
@@ -1,5 +1,7 @@
 ﻿$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 
+.\IncrementVersion-cs.ps1 ..\EmptyConsole
+
 $slnFilePath = "..\BuildSample.sln"
 Invoke-Expression ([string]::Format("{0} {1} /p:Configuration=Release /t:Clean", $msbuild, $slnFilePath))
 Invoke-Expression ([string]::Format("{0} {1} /p:Configuration=Release /t:Rebuild", $msbuild, $slnFilePath))
